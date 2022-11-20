ce5d31c67bd5cafc0061a09da363240b4a02baf8
Mutuma Gitonga <mgitonga150@gmail.com>
2022-11-20 03:15:04 +0300

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+  # get '/' do
+  #   { message: "Hello world" }.to_json
+  # end
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    
+    games.to_json
+
+    # {games: games, message: "There are #{games.count} games"}.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    
+    game.to_json(only: [:id, :title, :genre, :platform, :price], include: {reviews: {only: [:score, :comment], include: {user: {only: [:name]}}}})
+    # {games: games, message: "There are #{games.count} games"}.to_json
   end
 
 end
